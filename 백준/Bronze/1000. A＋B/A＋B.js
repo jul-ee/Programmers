29d4d46bf40b17751aaba4541cff6b6cf2a66a17
jul-ee <160218278+jul-ee@users.noreply.github.com>
2025-01-23 11:45:36 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,9 +1,8 @@
 let fs = require('fs');
-let input = fs.readFileSync('dev/stdin').toString().split(`\n`);
+let input = fs.readFileSync('/dev/stdin').toString().split(`\n`);
 
 let line = input[0].split(' ');
 
-let a = parseInt(line[0]);
-let b = parseInt(line[1]);
+let answer = parseInt(line[0]) + parseInt(line[1]);
 
-console.log(a + b);
+console.log(answer);
