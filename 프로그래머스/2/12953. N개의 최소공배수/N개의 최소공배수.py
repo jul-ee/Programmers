6f78d5d0ef3b485e749b864cfd1b73c22d553521
jul-ee <160218278+jul-ee@users.noreply.github.com>
2025-08-04 18:38:41 +0900

    [level 2] Title: N개의 최소공배수, Time: 62.30 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = arr[0]
+
+    for i in range(1, len(arr)):
+        for j in range(arr[i], answer * arr[i] + 1, arr[i]):
+            if j % answer == 0 and j % arr[i] == 0:
+                answer = j
+                break
+
+    return answer
