9cf43be0a18df2bbb05dcce7c48b22f9001f8e4d
jul-ee <160218278+jul-ee@users.noreply.github.com>
2025-08-28 16:56:41 +0900

    [level 2] Title: 더 맵게, Time: 1757.39 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(S, K):
+    heap = []
+    for i in S:
+        heapq.heappush(heap, i)
+
+    cnt = 0
+    while heap[0] < K:
+        try:
+            heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2)
+        except IndexError:
+            return -1
+        cnt += 1
+    return cnt
