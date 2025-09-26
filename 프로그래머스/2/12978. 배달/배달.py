9ea92453958d8dd840821fa21fbfe41b5561c9fc
jul-ee <160218278+jul-ee@users.noreply.github.com>
2025-09-26 23:01:13 +0900

    [level 2] Title: 배달, Time: 2.70 ms, Memory: 9.73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -0,0 +1,23 @@
+import heapq
+
+def dijkstra(dist,adj):
+    # 출발노드를 기준으로 각 노드들의 최소비용 탐색
+    heap = []
+    heapq.heappush(heap, [0,1])  # 거리,노드
+    while heap:
+        cost, node = heapq.heappop(heap)
+        for c,n in adj[node]:
+            if cost+c < dist[n]:
+                dist[n] = cost+c
+                heapq.heappush(heap, [cost+c,n])
+
+
+def solution(N, road, K):
+    dist = [float('inf')]*(N+1)
+    dist[1] = 0
+    adj = [[] for _ in range(N+1)]
+    for r in road:
+        adj[r[0]].append([r[2],r[1]])
+        adj[r[1]].append([r[2],r[0]])
+    dijkstra(dist,adj)
+    return len([i for i in dist if i <=K])
